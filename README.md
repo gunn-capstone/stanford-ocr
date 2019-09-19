447271c39c57709e433bbbcef8caa814274b48e2
ghostcat17 <34898169+ghostcat17@users.noreply.github.com>
2019-09-19 14:55:53 -0700

    initial commit
    Add files via upload
    
    Add files via upload
    
    new commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# StanfordSurveyImagetoText
+This is the API for the Stanford Surveys. 
+
+It will include an image to database software.
---
+++ StanfordSurveyImagetoText/src/main/java/Test.java
@@ -0,0 +1,69 @@
+import com.google.cloud.vision.v1.*;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.protobuf.ByteString;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args)throws Exception{
+        detectDocumentText("C:/Users/justi/StanfordSurveyImagetoText/src/main/resources/download.jpg",System.out);
+    }
+    public static void detectDocumentText(String filePath, PrintStream out) throws Exception,
+            IOException {
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+
+        ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
+
+        Image img = Image.newBuilder().setContent(imgBytes).build();
+        Feature feat = Feature.newBuilder().setType(Type.DOCUMENT_TEXT_DETECTION).build();
+        AnnotateImageRequest request =
+                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+        requests.add(request);
+
+        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+            client.close();
+
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    out.printf("Error: %s\n", res.getError().getMessage());
+                    return;
+                }
+
+                // For full list of available annotations, see http://g.co/cloud/vision/docs
+                TextAnnotation annotation = res.getFullTextAnnotation();
+                for (Page page: annotation.getPagesList()) {
+                    String pageText = "";
+                    for (Block block : page.getBlocksList()) {
+                        String blockText = "";
+                        for (Paragraph para : block.getParagraphsList()) {
+                            String paraText = "";
+                            for (Word word: para.getWordsList()) {
+                                String wordText = "";
+                                for (Symbol symbol: word.getSymbolsList()) {
+                                    wordText = wordText + symbol.getText();
+                                    out.format("Symbol text: %s (confidence: %f)\n", symbol.getText(),
+                                            symbol.getConfidence());
+                                }
+                                out.format("Word text: %s (confidence: %f)\n\n", wordText, word.getConfidence());
+                                paraText = String.format("%s %s", paraText, wordText);
+                            }
+                            // Output Example using Paragraph:
+                            out.println("\nParagraph: \n" + paraText);
+                            out.format("Paragraph Confidence: %f\n", para.getConfidence());
+                            blockText = blockText + paraText;
+                        }
+                        pageText = pageText + blockText;
+                    }
+                }
+                out.println("\nComplete annotation:");
+                out.println(annotation.getText());
+            }
+        }
+    }
+}
