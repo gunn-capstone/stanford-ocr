5ef88a46ed52d47cba002b18e29878ae1ad5a5b0
ghostcat17 <34898169+ghostcat17@users.noreply.github.com>
2019-09-26 14:43:21 -0700

    Add files via upload
    new commit

--- StanfordSurveyImagetoText/src/main/java/Test.java
+++ StanfordSurveyImagetoText/src/main/java/Test.java
@@ -1,4 +1,5 @@
 import com.google.cloud.vision.v1.*;
+import com.google.cloud.vision.v1.Feature.Type;
 import com.google.protobuf.ByteString;
 
 import java.io.FileInputStream;
@@ -8,16 +9,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
-    public static void main(String[] args){
-        detectText();
+    public static void main(String[] args)throws Exception{
+        detectDocumentText("C:/Users/justi/StanfordSurveyImagetoText/src/main/resources/download.jpg",System.out);
     }
-    public static void detectText(String filePath, PrintStream out) throws Exception, IOException {
+    public static void detectDocumentText(String filePath, PrintStream out) throws Exception,
+            IOException {
         List<AnnotateImageRequest> requests = new ArrayList<>();
 
         ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
 
         Image img = Image.newBuilder().setContent(imgBytes).build();
-        Feature feat = Feature.newBuilder().setType(Feature.Type.TEXT_DETECTION).build();
+        Feature feat = Feature.newBuilder().setType(Type.DOCUMENT_TEXT_DETECTION).build();
         AnnotateImageRequest request =
                 AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
         requests.add(request);
@@ -25,6 +27,7 @@ public class Test {
         try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
             BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
             List<AnnotateImageResponse> responses = response.getResponsesList();
+            client.close();
 
             for (AnnotateImageResponse res : responses) {
                 if (res.hasError()) {
@@ -33,12 +36,34 @@ public class Test {
                 }
 
                 // For full list of available annotations, see http://g.co/cloud/vision/docs
-                for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
-                    out.printf("Text: %s\n", annotation.getDescription());
-                    out.printf("Position : %s\n", annotation.getBoundingPoly());
+                TextAnnotation annotation = res.getFullTextAnnotation();
+                for (Page page: annotation.getPagesList()) {
+                    String pageText = "";
+                    for (Block block : page.getBlocksList()) {
+                        String blockText = "";
+                        for (Paragraph para : block.getParagraphsList()) {
+                            String paraText = "";
+                            for (Word word: para.getWordsList()) {
+                                String wordText = "";
+                                for (Symbol symbol: word.getSymbolsList()) {
+                                    wordText = wordText + symbol.getText();
+                                    out.format("Symbol text: %s (confidence: %f)\n", symbol.getText(),
+                                            symbol.getConfidence());
+                                }
+                                out.format("Word text: %s (confidence: %f)\n\n", wordText, word.getConfidence());
+                                paraText = String.format("%s %s", paraText, wordText);
+                            }
+                            // Output Example using Paragraph:
+                            out.println("\nParagraph: \n" + paraText);
+                            out.format("Paragraph Confidence: %f\n", para.getConfidence());
+                            blockText = blockText + paraText;
+                        }
+                        pageText = pageText + blockText;
+                    }
                 }
+                out.println("\nComplete annotation:");
+                out.println(annotation.getText());
             }
         }
     }
-
 }
