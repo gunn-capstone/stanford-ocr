2995260065605440bf4010cc5473acfb232ac0d8
Jonathan <jonathany66@yahoo.com>
2019-09-27 15:30:47 -0700

    quinn java update & license
    refactor
    
    Create LICENSE
    
    lens/cloud vision api example
    
    Update Detect.java
    
    Update Detect.java
    
    readme edit
    
    refactored quinn's code
    
    readme edit
    
    thing
    
    Update issue templates

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # StanfordSurveyImagetoText
-This is the API for the Stanford Surveys. 
+[![Discord][discord-shield]][discord-url]
+[![Issues][issues-shield]][issues-url]
 
-It will include an image to database software.
+This is a webapp by Gunn students for Stanford researchers to convert paper forms into digital data.
+
+# Links:
+
+[Google Cloud Vision](https://cloud.google.com/vision/docs/features-list)
+
+[discord-shield]:https://img.shields.io/discord/608526592444203038
+[discord-url]:https://discord.gg/M44c76X
+[issues-shield]:https://img.shields.io/github/issues/ghostcat17/StanfordSurveyImagetoText
+[issues-url]:https://github.com/ghostcat17/StanfordSurveyImagetoText/issues
---
+++ js/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// middleware 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+Grid = mongoose.Grid;
+
+mongoose.connect("mongodb://localhost:27017/node-demo", {useNewUrlParser: true }, function (err, db) {
+    if (err) return console.dir(err);
+
+	let grid = new Grid(db, 'fs');
+	let buffer = new Buffer("Hello world");
+	grid.put(buffer, {metadata: {category: 'text'}, content_type: 'text'}, function (err, fileInfo) {
+        grid.get(fileInfo._id, function (err, data) {
+            console.log("Retrieved data: " + data.toString());
+            grid.delete(fileInfo._id, function (err, result) {
+            });
+        });
+    });
+});
+
+
+const nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastNameName: String
+});
+
+const User = mongoose.model("User", nameSchema);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+// node app.js
+
+app.post("/addname", (req, res) => {
+	let myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("item saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("unable to save to database");
+        });
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
