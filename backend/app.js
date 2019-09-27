c53ce61859a991094ff02cdcacc5ebfe0f7d24a7
Jonathan <jonathany66@yahoo.com>
2019-09-27 15:30:47 -0700

    car

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
+
+Grid = mongo.Grid;
+
 // mongoose.connect("mongodb://localhost:27017/node-demo");
-mongoose.connect("mongodb://localhost:27017/node-demo", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/node-demo", { useNewUrlParser: true });
+
+mongoose.connect("mongodb://localhost:27017/node-demo", function(err, db) {
+	if(err) return console.dir(err);
+
+	var grid = new Grid(db, 'fs');
+	var buffer = new Buffer("Hello world");
+	grid.put.(buffer, {metadata:{category:'text'}, content_type: 'text'}, function(err, fileInfo) {
+		grid.get(fileInfo._id, function(err, data) {
+			console.log("Retrieved data: " + data.toString());
+			grid.delete(fileInfo._id, function(err, result) {
+			});
+		});
+	});
+});
+
 
 var nameSchema = new mongoose.Schema({
 	firstName: String,
