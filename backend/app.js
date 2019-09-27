ab424d0900844db19c9b3c9101a0ff21cd3074a7
Jonathan <jonathany66@yahoo.com>
2019-09-27 14:32:17 -0700

    backend init

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+
+var express = require("express");
+var app = express();
+var port = 3000;
+
+// middleware 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+// mongoose.connect("mongodb://localhost:27017/node-demo");
+mongoose.connect("mongodb://localhost:27017/node-demo", { useNewUrlParser: true });
+
+var nameSchema = new mongoose.Schema({
+	firstName: String,
+	lastNameName: String
+});
+
+var User = mongoose.model("User", nameSchema);
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+// node app.js
+
+app.post("/addname", (req, res) => {
+	var myData = new User(req.body);
+	myData.save()
+	.then(item => {
+		res.send("item saved to database");
+	})
+	.catch(err => {
+		res.status(400).send("unable to save to database");
+	});
+});
+
+app.listen(port, () => {
+	console.log("Server listening on port " + port);
+});
