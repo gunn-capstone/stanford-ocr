57444855d6ad373c2fdcbf20d6e626e0c810812c
Clement Ou <clement.h.ou@gmail.com>
2019-10-01 14:50:51 -0700

    cleanup
    and i oop
    
    maybe this will work idk

--- js/server.js
+++ js/server.js
@@ -1,29 +1,17 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
-// middleware 
+// middleware
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-Grid = mongoose.Grid;
-
-mongoose.connect("mongodb://localhost:27017/node-demo", {useNewUrlParser: true }, function (err, db) {
-    if (err) return console.dir(err);
-
-	// let grid = new Grid(db, 'fs');
-	// let buffer = new Buffer("Hello world");
-	// grid.put(buffer, {metadata: {category: 'text'}, content_type: 'text'}, function (err, fileInfo) {
-    //     grid.get(fileInfo._id, function (err, data) {
-    //         console.log("Retrieved data: " + data.toString());
-    //         grid.delete(fileInfo._id, function (err, result) {
-    //         });
-    //     });
-    // });
-});
+mongoose.connect("mongodb://localhost:27017/node-demo",
+    {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true}).then(r => {}) ;
 
 
 const nameSchema = new mongoose.Schema({
@@ -39,7 +27,7 @@ app.get("/", (req, res) => {
 // node app.js
 
 app.post("/addname", (req, res) => {
-	let myData = new User(req.body);
+    let myData = new User(req.body);
     myData.save()
         .then(item => {
             res.send("item saved to database");
@@ -49,7 +37,14 @@ app.post("/addname", (req, res) => {
         });
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`);
+app.get('/data', function (req, res,html) {
+    res.sendFile(__dirname+'/data.html');
+});
+
+app.get('/index', function (req, res,html) {
+    res.sendFile(__dirname+'/index.html');
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
 });
