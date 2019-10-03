53d6f312855babd13fa6c19c552796de1e3b59dc
Clement Ou <clement.h.ou@gmail.com>
2019-10-03 13:50:08 -0700

    firestore

--- js/server.js
+++ js/server.js
@@ -1,42 +1,40 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
+const admin = require('firebase-admin');
 
-const mongodb = require('mongodb');
-const http = require('http');
-const nconf = require('nconf');
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://<stanford-ocr>.firebaseio.com'
+});
 
-// middleware
+const db = admin.firestore();
+const pdfCollection = db.collection('pdfCollection');
+const dataCollection = db.collection('dataCollection');
+
+function hashCode (s){
+    let h = 0, l = s.length, i = 0;
+    if ( l > 0 )
+        while (i < l)
+            h = (h << 5) - h + s.charCodeAt(i++) | 0;
+    return h;
+}
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-
-mongoose.connect("mongodb://localhost:27017b/node-demo",
-    {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true}).then(r => {}) ;
-
-const nameSchema = new mongoose.Schema({
-    firstName: String,
-    lastNameName: String
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
-const User = mongoose.model("User", nameSchema);
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
-
-app.post("/addname", (req, res) => {
-    let myData = new User(req.body);
-    myData.save()
-        .then(item => {
-            res.send("item saved to database");
-        })
-        .catch(err => {
-            res.status(400).send("unable to save to database");
-        });
+app.post('/addname', (req, res) => {
+    console.log('posted');
+    let dataRef = dataCollection.doc(hashCode(req.getElementById('lastName')).toString());
+    let setData = dataRef.set({
+        lastName: req.getElementById('lastName'),
+        firstName: req.getElementById('firstName')
+    }, {merge:true});
 });
 
 app.get('/data', function (req, res,html) {
@@ -48,5 +46,5 @@ app.get('/index', function (req, res,html) {
 });
 
 app.listen(port, () => {
-    console.log("Server listening on port " + port);
+    console.log('Server listening on port ' + port);
 });
