efdcc0518d4e5ef9b4133499f655fcbbbfe1124a
Clement Ou <clement.h.ou@gmail.com>
2019-10-11 18:33:40 -0700

    form

--- js/app.js
+++ js/app.js
@@ -11,16 +11,7 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
-const pdfCollection = db.collection('pdfCollection');
 const dataCollection = db.collection('dataCollection');
-
-function hashCode(s) {
-    let h = 0, l = s.length, i = 0;
-    if (l > 0)
-        while (i < l)
-            h = (h << 5) - h + s.charCodeAt(i++) | 0;
-    return h;
-}
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -31,11 +22,19 @@ app.get('/', (req, res) => {
 });
 
 
-app.post('/addname', (req, res) => {
-    let dataRef = dataCollection.doc(hashCode(req.body.lastName).toString());
+app.post('/addparticipant', (req, res) => { // TODO data validation
+    let dataRef = dataCollection.doc(req.body.id);
     let setData = dataRef.set({
-        lastName: req.body.lastName,
-        firstName: req.body.firstName
+        id: req.body.id, // TODO for loop
+        age: req.body.age,
+        gender: req.body.gender,
+        specialty: req.body.specialty,
+        years: req.body.years,
+        percent: req.body.percent,
+        activities: req.body.activities,
+        additional: req.body.additional,
+        name: req.body.name,
+        email: req.body.email
     }, {merge: true});
     res.send('gay');
 });
