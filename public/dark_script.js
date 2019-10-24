47cff733307a7cf93566847c9d30be05e3294238
Jonathan Yin <jonathanyin66@gmail.com>
2019-10-24 15:20:32 -0700

    yes
    Signed-off-by: Jonathan Yin <jonathanyin66@gmail.com>

--- public/dark_script.js
+++ public/dark_script.js
@@ -1,7 +1,9 @@
 $('#switch').on('click', () => {
     if ($('#switch').prop('checked')) {
         $('.wrapper').addClass('dark');
+        $('.wrapper').addClass('white');
     } else {
         $('.wrapper').removeClass('dark');
+        $('.wrapper').removeClass('white');
     }
 })
