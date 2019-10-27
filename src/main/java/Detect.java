8e72c1cbba56d06e666deae5d437b5f9708536a9
Clement Ou <clement.h.ou@gmail.com>
2019-10-26 17:29:13 -0700

    added error handling to make it kabir-proof

--- src/main/java/Detect.java
+++ src/main/java/Detect.java
@@ -1,10 +1,9 @@
-// Imports the Google Cloud client library
-
 import com.google.cloud.vision.v1.*;
 import com.google.cloud.vision.v1.Image;
 import com.google.cloud.vision.v1.Feature.Type;
 import com.google.protobuf.ByteString;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
@@ -13,33 +12,57 @@ import java.util.List;
 
 public class Detect {
     public static void main(String[] args) throws Exception { // dont use absolute paths >:(
-        detectText(System.getProperty("user.dir") + "/src/main/resources/image 3.jpg", new PrintStream(System.getProperty("user.dir") + "/src/main/resources/output.txt"));
+        String input = System.getProperty("user.dir");
+        if (args.length > 0 && args[0] != null) {
+            input += args[0];
+        } else {
+            input += "/src/main/resources/image 3.jpg";
+            System.out.println("No input file specified, default(" + input + ") set as input");
+        }
+
+        String output = System.getProperty("user.dir");
+        if (args.length > 0 && args[1] != null) {
+            output += args[1];
+        } else {
+            output += "/src/main/resources/output.txt";
+            System.out.println("No output file specified, default(" + output + ") set as output");
+        }
+
+        try {
+            detectText(input, new PrintStream(output));
+        } catch (FileNotFoundException e) {
+            File outputFile = new File(output);
+            if (outputFile.createNewFile()) {
+                System.out.println(input + " does not exist");
+                throw e;
+            } else {
+                detectText(input, new PrintStream(output));
+            }
+        }
+
     }
 
 
-/*
-private static File cropImage(Rectangle rect, String pathFile) throws Exception{
+    private static File cropImage(Rectangle rect, String pathFile) throws Exception {
+        BufferedImage image = ImageIO.read(new File(pathFile)).getSubimage(rect.x, rect.y, rect.width, rect.height);
 
-    BufferedImage image = ImageIO.read(new File(pathFile)).getSubimage(rect.x, rect.y, rect.width, rect.height);
-
-    File outputfile = new File("image.jpg");
-    ImageIO.write(image, "jpg", outputfile);
-    try{
+        File outputFile = new File("image.jpg");
+        ImageIO.write(image, "jpg", outputFile);
+        try {
 
 
-        image.getGraphics().drawLine(1, 1, image.getWidth()-1, image.getHeight()-1);
-        image.getGraphics().drawLine(1, image.getHeight()-1, image.getWidth()-1, 1);
+            image.getGraphics().drawLine(1, 1, image.getWidth() - 1, image.getHeight() - 1);
+            image.getGraphics().drawLine(1, image.getHeight() - 1, image.getWidth() - 1, 1);
 
-        ImageIO.write(image, "jpg", "./resources/untitled.jpg");
+            ImageIO.write(image, "jpg", new File("./resources/untitled.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return outputFile;
     }
-    catch (IOException e){
-        e.printStackTrace();
-    }
-    return outputfile;
-}*/
 
 
-    public static void detectText(String filePath, PrintStream out) throws Exception, IOException {
+    private static void detectText(String filePath, PrintStream out) throws Exception {
         List<AnnotateImageRequest> requests = new ArrayList<>();
 
         ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
