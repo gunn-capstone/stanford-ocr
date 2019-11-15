fb6d30ee248ae28163442ec0741494009c656b5c
Jonathan Yin <jonathanyin66@gmail.com>
2019-11-15 14:35:19 -0800

    implementing csv export, changing UI
    Signed-off-by: Jonathan Yin <jonathanyin66@gmail.com>

--- src/app.js
+++ src/app.js
@@ -44,6 +44,34 @@ app.get('/test', function (req, res) { // huh why this no work
     res.send('huh');
 });
 
+const json2csv = require("json2csv").parse;
+
+exports.csvJsonReport = functions.https.onRequest((request, response) => {
+
+  const db = admin.firestore();
+  const ordersRef = db.collection('dataCollection');
+  return ordersRef.get()
+    .then((querySnapshot) => {
+      const dataCollection = [];
+
+      querySnapshot.forEach(doc => {
+        const order = doc.data();
+        dataCollection.push(order);
+      });
+      const csv = json2csv(dataCollection);
+      response.setHeader(
+        "Content-disposition",
+        "attachment; filename=report.csv"
+      );
+      response.set("Content-Type", "text/csv");
+      response.status(200).send(csv)
+    }).catch((err) => {
+      console.log(err);
+    });
+
+});
+
+
 const port = process.env.port || 8080;
 app.listen(port, () => {
     console.log("Server listening on port " + port);
